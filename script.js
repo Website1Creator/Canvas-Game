b9f42bb1b1c105a8e45a7d032a78299aaef485c5
Website1Creator <61760572+Website1Creator@users.noreply.github.com>
2021-08-10 13:10:21 +1200

    Added new characters and made one walk
    Did a bit of work to make it refresh and move around, also added rotation however that is definitely wip. I will add a second character next and make him walk, then will be rotation, enemies and shooting.

--- script.js
+++ script.js
@@ -6,54 +6,75 @@ canvas.height = innerHeight
 const widthCenter = canvas.width / 2
 const heightCenter = canvas.height / 2
 const widthQuarter = canvas.width / 4
+var pSize = canvas.height / 20
+var playerMov = {x:0, y:0}
 
 //Create Players
-function Players(wq, hc, ctx) {
-    Player1 = document.getElementById("Player1")
-    ctx.drawImage(Player1, wq - 25, hc, 50, 100)
+Player1 = document.getElementById("Player1")
+Player2 = document.getElementById("Player2")
+ctx.drawImage(Player2, (widthQuarter * 3) - 25, heightCenter, 50, 100)
 
-    Player2 = document.getElementById("Player2")
-    ctx.drawImage(Player2, (wq * 3) - 25, hc, 50, 100)
-}
 
 //In Charge Of Player Movement
-class playerMove {
-    constructor(wc, hc) {
-        this.playerMov = {
-            x: 0, 
-            y: 0
-        }
+class playerMove1 {
+    constructor(pSize, pl1, ctx, wq, sh, wc, hc, playerMov) {
+        this.playerMov = playerMov
+        this.sh = sh
         this.wc = wc
         this.hc = hc
+        this.wq = wq
+        this.x = wq - 25
+        this.y = hc
+        this.ctx = ctx
+        this.pl1 = pl1
+        this.pSize = pSize
+        this.angle = 0
+        this.angleX = 0
     }
     moveLeft() {
-        this.playerMov.x = -1
+        this.playerMov.x = -2
     }
     moveRight() {
-        this.playerMov.x = 1
+        this.playerMov.x = 2
     }
     moveForward() {
-        this.playerMov.y = 1
+        this.playerMov.y = -2
     }
     moveBackward() {
-        this.playerMov.y = -1
+        this.playerMov.y = 2
     }
     rotateR() {
-
+        this.angleX= 0.001
     }
     rotateL() {
-
+        this.angleX = -0.001
     }
-    stop() {
+    stopX() {
         this.playerMov.x = 0
+    }
+    stopY() {
         this.playerMov.y = 0
     }
     stopR() {
-
+        this.angle = 0
+        this.angleX = 0
+    }
+    draw() {
+        ctx.rotate(this.angle)
+        this.ctx.drawImage(this.pl1, this.x, this.y, this.pSize, this.pSize)
+    }
+    update() {
+        this.draw()
+        this.angle = this.angle + this.angleX 
+        this.x = this.x + this.playerMov.x
+        this.y = this.y + this.playerMov.y
+        if(this.x > this.wc - this.pSize) this.x = this.wc - this.pSize
+        if(this.x < 0) this.x = 0
+        if(this.y < 0) this.y = 0
+        if(this.y > this.sh - this.pSize) this.y = this.sh - this.pSize
     }
 }
 
-let playerMov
 
 //Players gun
 class playerGun {
@@ -66,7 +87,7 @@ class playerGun {
 }
 
 //Get Inputs
-class input {
+class input1 {
     constructor(playerMove, playerGun) {
         document.addEventListener('keydown', (event) => {
             switch(event.keyCode) {
@@ -103,19 +124,19 @@ class input {
         document.addEventListener('keyup', (event) => {
             switch(event.keyCode) {
                 case 65:
-                    playerMove.stop()
+                    playerMove.stopX()
                     break;
  
                 case 87:
-                    playerMove.stop()
+                    playerMove.stopY()
                     break;
 
                 case 68:
-                    playerMove.stop()
+                    playerMove.stopX()
                     break;
 
                 case 83:
-                    playerMove.stop()
+                    playerMove.stopY()
                     break;
 
                 case 67:
@@ -149,20 +170,22 @@ class UI {
         this.ctx.stroke()
     }
 }
-playermove = new playerMove(widthCenter, heightCenter)
+playermove1 = new playerMove1(pSize, Player1, ctx, widthQuarter, innerHeight, widthCenter, heightCenter, playerMov)
 playergun = new playerGun()
-new input(playermove, playergun)
-Players(widthQuarter, heightCenter, ctx)
+new input1(playermove1, playergun)
 //For Left Canvas
 ui = new UI(ctx, innerHeight, widthQuarter, widthCenter, innerWidth)
-ui.centerLine()
+
+let lastTime = 0
+
 function gameLoop(timestamp) {
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
 
-    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-    game.update(deltaTime);
-    game.draw(ctx);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    playermove1.update(deltaTime);
+    // playermove2.update(deltaTime);
+    ui.centerLine()
 
     requestAnimationFrame(gameLoop);
 }
